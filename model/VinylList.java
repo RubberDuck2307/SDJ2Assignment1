75293b55c79c70a88b998e55cbdfbc07005077e6
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-02-28 10:13:12 +0100

    VinylList done

---
+++ model/VinylList.java
@@ -0,0 +1,81 @@
+package model;
+
+import java.util.ArrayList;
+
+public class VinylList
+{
+  private ArrayList<Vinyl> vinylList;
+
+  public VinylList()
+  {
+    vinylList = new ArrayList<Vinyl>();
+  }
+
+  public void addVinyl(Vinyl vinyl)
+  {
+    vinylList.add(vinyl);
+  }
+
+  public void removeVinyl(Vinyl vinyl)
+  {
+    vinylList.remove(vinyl);
+  }
+
+  public void removeVinylByIndex(int index)
+  {
+    vinylList.remove(index);
+  }
+
+  public void removeVinylByID(int ID)
+  {
+    int index = -1;
+    for (int i = 0; i < vinylList.size(); i++)
+    {
+      vinylList.get(i).getID() == ID;
+      index = i;
+    }
+    vinylList.remove(index);
+  }
+
+  public ArrayList<Vinyl> getVinylList()
+  {
+    return vinylList;
+  }
+
+  public Vinyl getVinylByIndex(int index)
+  {
+    return vinylList.get(index);
+  }
+
+  public Vinyl getVinylByTitle(String title)
+  {
+    int index = -1;
+    for (int i = 0; i < vinylList.size(); i++)
+    {
+      vinylList.get(i).getTitle().equals(title);
+      index = i;
+    }
+    return vinylList.get(index);
+  }
+
+  public Vinyl getVinylByID(int ID)
+  {
+    int index = -1;
+    for (int i = 0; i < vinylList.size(); i++)
+    {
+      vinylList.get(i).getID() == ID;
+      index = i;
+    }
+    return vinylList.get(index);
+  }
+
+  public int getSize()
+  {
+    return vinylList.size();
+  }
+
+  @Override public String toString()
+  {
+    return "VinylList{" + "vinylList=" + vinylList + '}';
+  }
+}
