474a968b4058d2e55b485defe7a0e4932a46396e
Jan Metela <janmetela@seznam.cz>
2023-02-28 10:59:03 +0100

    View State

---
+++ view/ViewState.java
@@ -0,0 +1,17 @@
+package view;
+
+import model.Vinyl;
+
+public class ViewState {
+
+    Vinyl vinyl;
+
+
+    public Vinyl getVinyl() {
+        return vinyl;
+    }
+
+    public void setVinyl(Vinyl vinyl) {
+        this.vinyl = vinyl;
+    }
+}
